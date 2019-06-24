c4ee810045a19885c1554b529d3f0a7195d45627
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2019-06-24 22:37:18 +0900

    add example

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,13 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import com.typesafe.config.ConfigFactory
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+object Hello extends App {
+  val conf = ConfigFactory.load
+  println("conf.getString(\"simple\") is " + conf.getString("simple"))
+  println("conf.getString(\"definedInAnother\") is " + conf.getString("definedInAnother"))
+  println("conf.getString(\"mergedFromAnother\") is " + conf.getString("mergedFromAnother"))
+  println("conf.getString(\"application.mergedFromAnother\") is " + conf.getString("application.mergedFromAnother"))
+  println("conf.getString(\"application.additionalFromAnother\") is " + conf.getString("application.additionalFromAnother"))
+  println("conf.getString(\"application.additionalFromApplication\") is " + conf.getString("application.additionalFromApplication"))
 }
